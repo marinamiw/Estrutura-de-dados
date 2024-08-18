845c45c2cac7fd56868fe8c51b1dec48615b5546
Marina <111240861+marinamiw@users.noreply.github.com>
2024-08-18 12:06:21 -0300

    Update listaEnc.c
    conserto de erros

--- Listas encadeadas/listaEnc.c
+++ Listas encadeadas/listaEnc.c
@@ -1,237 +1,366 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
-//Exemplo de Lista Encadeada usando nó cabeça
 
 typedef struct no{
-int dado;
-struct no *proxNo;
-}tipoNo;
+    int dado;
+    struct no *proxNo;
+}TipoNo;
 
-typedef struct listaGerenciada{
-  tipoNo *inicio;
-  tipoNo *fim;
-  int quant;
-  }tipoLista;
+typedef struct lista{
+    TipoNo *inicio;
+    TipoNo *final;
+    int tamanho;
+}TipoLista;
 
-void inicializa(tipoLista *lista){
+void inicializaLista(TipoLista *lista){
     lista->inicio = NULL;
-    lista->fim = NULL;
-    lista->quant = 0;
+    lista->final = NULL;
+    lista->tamanho = 0;
 }
 
-
-bool removerNoInicio(tipoLista *lista){
-
-
-    if (lista->inicio == NULL)
-        return false;
-
-
-    // definir o que é anterior (ele ira guardar o primeiro nó de fato)
-
-    tipoNo* noAnterior = lista->inicio;
-    //agora o inicio sera o proximo no q veio dps dele. 
-    lista->inicio = lista->inicio->proxNo; //agora define que o inicio eh o segundo elemento
-    free(noAnterior); //limpo o no anterior que guardava o primeiro no, removendo ele da lista.
-    lista->quant--;
-    return true;
+bool insereListaVazia(TipoLista *lista, int valor){ // caso a lista esteja vazia ele insere um elemento
+    TipoNo *NovoNo;
+    NovoNo = (TipoNo*) malloc(sizeof(NovoNo));
     
-}
-
-bool removerNoFinal(tipoLista*lista){
-
-    if(lista->inicio == NULL)
+    if (NovoNo == NULL){
         return false;
-
-    tipoNo* anterior;
-    anterior->proxNo = NULL; //guarda um valor para ser o novo 'ultimo' ,uma vez que o ultimo nó for apagado. o ultimo valor sempre aponta para o nulo.
-   
-    free(lista->fim); //apaga o ultimo No de fato
-    lista->fim = anterior; //atualiza o ponteiro final da lista para o no que foi guardado, tornando ele o ultimo
-    lista->quant--;
-    return true;
-
-}
-
-void destruir(tipoLista*lista){
-
-    if(lista->inicio == NULL)
-        return;
-    
-    tipoNo* atual = lista->inicio;
-   
-    while(atual != NULL) //lista percorrida ate que o atual seja nulo,indicando seu fim
-    {
-        tipoNo* prox = atual->proxNo; //guardando o proximo
-        free(atual); //limpando o atual
-        atual = prox; //passando para o proximo
     }
-    printf("Lista destruída com sucesso");
-    lista->inicio = NULL;
-    lista->quant = 0;
 
+    NovoNo->dado = valor;
+    NovoNo->proxNo = NULL;
+    lista->inicio = NovoNo;
+    lista->final = NovoNo;
+    lista->tamanho++;
+    return true;
 }
 
 
-int buscarItem(tipoNo* item, tipoLista*lista){
+bool insereInicio(TipoLista *lista, int valor){
+    TipoNo *NovoNo;
+    NovoNo = (TipoNo*) malloc(sizeof(NovoNo));
 
-    tipoNo* atual = lista->inicio;
-    tipoNo* anterior = NULL;
+    if(NovoNo == NULL){
+        return false;
+    }
 
-int i = 0;
+    if(lista->inicio == NULL){
+        insereListaVazia(lista,valor);
+    }
 
+    NovoNo->dado = valor;
+    NovoNo->proxNo = lista->inicio;
+    lista->inicio = NovoNo;
+    lista->tamanho++;
+    return true;
+}
+
+bool insereFinal(TipoLista *lista, int valor){
+    TipoNo *NovoNo;
+    NovoNo = (TipoNo*) malloc(sizeof(TipoNo));
+    
+    if (NovoNo == NULL){
+        return false;
+    }
+
+    if(lista->inicio == NULL){
+        insereListaVazia(lista, valor);
+        return true;
+    }
+
+    NovoNo->dado = valor;
+    NovoNo->proxNo = NULL;
+    lista->final->proxNo = NovoNo;
+    lista->final = NovoNo;
+    lista->tamanho++;
+    return true;
+}
+
+void exibeLista(TipoLista *lista){
+    TipoNo *atual;
+    if(lista->inicio == NULL){
+        printf("A lista esta vazia.");
+        exit(0);
+    }
+    atual = lista->inicio;
     while(atual != NULL){
+        printf("%d",atual->dado);
+        atual = atual->proxNo;
+    }
+}
 
-        if (atual->dado == item){
-            printf("item encontrado na posicao %d\n",i);   //se o dado do nó atual corresponder ao valor interessado, ok
+bool removerInicio(TipoLista *lista){
+    
+    if(lista->inicio == NULL){
+        printf("A lista esta vazia.");
+        return false;
+    }
+
+    TipoNo *AntigoInicio;
+    AntigoInicio = lista->inicio;
+    lista->inicio = lista->inicio->proxNo;
+    free(AntigoInicio);
+    lista->tamanho--;
+    return true;
+}
+
+bool removerFinal(TipoLista *lista){
+    
+    if (lista->inicio == NULL){
+        printf("a lista esta vazia");
+        return false;
+    }
+
+    TipoNo *anterior;
+    anterior = lista->final;
+    anterior->proxNo = NULL;
+    free(lista->final);
+    lista->tamanho--;
+    return true;
+
+}
+
+bool buscaLista(TipoLista *lista, int posicao){
+    TipoNo *atual;
+    atual = lista->inicio;
+    int contador = 0;
+    while(atual != NULL){
+        if(contador == posicao){
+            printf("Posicao encontrada. Seu valor é: %d",atual->dado);
+            return true;
         }
-        
+        atual = atual->proxNo;
+        contador++;
+    }
+}
+
+bool inserirNaPos(TipoLista *lista, int posicao, int valor){
+
+    TipoNo *NovoNo;
+    NovoNo = (TipoNo*) malloc(sizeof(TipoNo));
+    TipoNo *atual;
+    TipoNo *anterior;
+    atual = lista->inicio;
+    anterior = NULL;
+    NovoNo->dado = valor;
+    int contador = 1;
+
+    if(posicao < 0 || posicao > lista->tamanho){
+        printf("posicao invalida. insira outra");
+        return false;
+    }
+
+
+    if(NovoNo == NULL){
+        return false;
+    }
+
+    if(lista->inicio == NULL){
+        insereListaVazia(lista, valor);
+        return true;
+    }
+
+    if (posicao == 0){
+        insereInicio(lista,valor);
+    }
+    
+    while(atual != NULL && contador < posicao){
         anterior = atual;
         atual = atual->proxNo;
-        
-        i++;
+        contador++;
+        }
+
+    if(contador == posicao){
+        NovoNo->proxNo = atual;
+        anterior->proxNo = NovoNo;
+        lista->tamanho++;
+        return true;
     }
-    
-    if (atual == NULL){
-        printf("Nao existe esse item na lista dada");
-    }
-    
+
+    free(NovoNo);
+    return false;
 }
 
-bool RemoverPosicao(tipoNo *posicao, tipoLista*lista){
+bool removerNaPos(TipoLista *lista, int pos,int valor){
+
+    TipoNo *atual,*anterior;
+
+    if(lista->inicio == NULL){
+        return false;
+    }
+
+    if(pos < 0 || pos >= lista->tamanho){
+        printf("posicao invalida");
+        return false;
+    }
+
+    if(pos == 0){
+        removerInicio(lista);
+        return true;
+    }
+
+    atual = lista->inicio;
+    anterior = NULL;
+    int contador = 0;
+    while (atual != NULL && contador < pos){
+        anterior = atual;
+        atual = atual->proxNo;
+        contador++;
+    }
+
+    if(atual == NULL){
+        return false;
+    }
+
+
+    if(contador == pos){
+        anterior->proxNo = atual->proxNo;
+        free(atual);
+        lista->tamanho--;
+        return true;
+    }
+
+return false;
+}
+
+bool destruirlista(TipoLista *lista){
 
     if (lista->inicio == NULL){
-        return false;
-    }
-    
-    int i = 0; //contador para acompanhar a posiçao no loop
-    tipoNo *atual = lista->inicio;
-    tipoNo *anterior = NULL;
-    
-    while(atual != NULL){
-       
-        if(i == posicao){
-            anterior->proxNo = atual->proxNo;  // como estamos removendo um nó, precisamos que o anterior desse nó agora aponte para o próximo desse nó, uma vez que deixaria um 'vacuo' entre eles qd removido
-            free(atual);
-            printf("item removido com sucesso na posicao dada");
-            return true;
-        }
-        anterior = atual;
-        atual = atual->proxNo;
-        i++;
-    }
-    lista->quant--;
-}
-
-
-bool inserirPosicao(tipoNo *posicao, tipoLista *lista){
-
-     if (lista->inicio == NULL){
+        printf("A lista ta vazia");
         return false;
     }
 
-    int i = 0;
-    tipoNo *atual = lista->inicio;
-    tipoNo *anterior = NULL;
-
+    TipoNo *atual;
+    TipoNo *prox;
+    atual = lista->inicio;
+    
     while(atual != NULL){
-
-        if (i == posicao){
-            posicao->proxNo = atual; // quando o nó for inserido na posicao desejada, queremos que o nó novo aponte para o no q estava na posicao anteriormente 
-                                            // e o anterior desse mesmo no aponte para o no inserido e nao mais para o antigo
-            anterior->proxNo = posicao;
-            return true;
-            printf("Item adicionado com sucesso na posicao");
-        }
-        anterior = atual;
-        atual = atual->proxNo;
-        i++;
+        prox = atual->proxNo;
+        free(atual);
+        atual = prox;
     }
-    lista->quant++;
-
+    
+    lista->inicio = NULL;
+    lista->tamanho = 0;
+    return true;
 }
 
+bool insereOrdenado(TipoLista *lista, int valor){
 
-int insereListaVazia(tipoLista *lista, int valor){
-tipoNo *novoNo = (tipoNo*) malloc(sizeof(tipoNo));
-if(novoNo == NULL)
-    return 0;
-novoNo->dado = valor;
-novoNo->proxNo = NULL;
-lista->inicio = novoNo;
-lista->fim = novoNo;
-lista->quant++;
-}
+    TipoNo *novoNo;
+    novoNo = (TipoNo*) malloc(sizeof(TipoNo));
+    
+    if(novoNo == NULL){
+        return false;
+    }
 
-int insereNaFrente(tipoLista *lista, int valor){
-tipoNo *novoNo;
-if(lista->inicio == NULL)
-    insereListaVazia(&lista ,valor);
-else{
-    novoNo = (tipoNo*)malloc(sizeof(tipoNo));
-    if(novoNo==NULL)
-        return 0;
     novoNo->dado = valor;
-    novoNo->proxNo = lista->inicio;
-    lista->inicio = novoNo;
-    return 1;
-}
-}
-
-/*Fun��o de exibi��o da lista simplesmente encadeada*/
-int exibeLista(tipoLista *lista){
-tipoNo *atual;  //Vari�vel que ser� usada para percorrer a lista*/
-if(lista->inicio == NULL)
-    return 0;
-atual = lista->inicio;
-printf("\nLista encadeada: ");
-while(atual !=NULL){
-    printf("%8d",atual->dado);
-    atual = atual->proxNo;
+    TipoNo *atual, *anterior;
+    atual = lista->inicio;
+    anterior = NULL;
+    
+    if(valor < atual->dado){
+        insereInicio(lista,valor);
+        return true;
     }
-return 1;
+
+    while(atual->dado < valor && atual->proxNo != NULL){
+        anterior = atual;
+        atual = atual->proxNo;
+    }
+    if(atual->dado > novoNo->dado){
+        anterior->proxNo = novoNo;
+        novoNo->proxNo = atual;
+        lista->tamanho++;
+
+    }
+    else{
+        insereFinal(lista,valor);
+    }
+return true;
 }
 
-void main(){
-tipoLista lista;
-inicializa(&lista);
-int leitor;
-printf("Digite um elemento: ");
-scanf("%d",&leitor);
-int resp = insereListaVazia(&lista, leitor);
-
-if(!resp)
-   printf("Memoria nao alocada!");
-printf("Elemento guardado: %d",lista.inicio->dado);
-
-printf("\nDigite outro elemento: ");
-scanf("%d",&leitor);
-resp = insereNaFrente(&lista,leitor);
-exibeLista(&lista);
-
-printf("Digite uma lista: ");
-scanf("%d",&lista);
-resp = removerNoInicio(&lista);
-
-printf("Digite uma lista: ");
-scanf("%d",&lista);
-resp = removerNoFinal(&lista);
-
-printf("Digite uma lista: ");
-scanf("%d",&lista);
-destruir(&lista);
-
-printf("Digite um valor que deseja buscar: ");
-scanf("%d",&leitor);
-resp = buscarItem(leitor,&lista);
-
-printf("Digite uma posicao que deseja inserir na lista: ");
-scanf("%d",&leitor);
-resp = inserirPosicao(leitor,&lista);
-
-printf("Digite uma posicao que deseja remover da lista: ");
-scanf("%d",&leitor);
-resp = RemoverPosicao(leitor,&lista);
 
 
+
+int main(){
+
+    TipoLista lista;
+    inicializaLista(&lista);
+    int opcao, pos, valor;
+
+
+do {
+        printf("\nO que deseja fazer?");
+        printf("\n1- Adicionar valor");
+        printf("\n2- Remover valor");
+        printf("\n3- Destruir lista");
+        printf("\n4- Buscar por posição");
+        printf("\n5- Exibir lista");
+        printf("\n6- Sair");
+        printf("\nDigite sua opção: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+
+            case 1:
+                printf("\nDigite o valor que deseja adicionar: ");
+                scanf("%d", &valor);
+                printf("\nDigite a posição onde deseja adicionar: ");
+                scanf("%d", &pos);
+                if (inserirNaPos(&lista, pos, valor)) {
+                    printf("\nValor adicionado com sucesso!");
+                } else {
+                    printf("\nErro ao adicionar valor.");
+                }
+                break;
+
+            case 2:
+                printf("\nDigite a posição do valor que deseja remover: ");
+                scanf("%d", &pos);
+                if (removerNaPos(&lista, pos, valor)) {
+                    printf("\nValor removido com sucesso!");
+                } else {
+                    printf("\nErro ao remover valor.");
+                }
+                break;
+
+            case 3:
+                if (destruirlista(&lista)) {
+                    printf("\nLista destruída com sucesso!");
+                } else {
+                    printf("\nErro ao destruir a lista ou a lista já está vazia.");
+                }
+                break;
+
+            case 4:
+                printf("\nDigite a posição que deseja buscar: ");
+                scanf("%d", &pos);
+                if (!buscaLista(&lista, pos)) {
+                    printf("\nPosição não encontrada.");
+                }
+                break;
+
+            case 5:
+                printf("\nLista: ");
+                exibeLista(&lista);
+                break;
+
+            case 6:
+                printf("\nSaindo...");
+                break;
+
+            default:
+                printf("\nOpção inválida!");
+                break;
+        }
+    } while (opcao != 6);
+
+
+
+    getchar();
+    getchar();
+
+
+    return 0;
 }
